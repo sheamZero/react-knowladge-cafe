88e8de7c4eb6f94bee448d3802ab3dd631c1e09d
sheamZero <sheamshadik078@gmail.com>
2024-03-15 09:41:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <h1 className='text-7xl bg-red-400 font-extrabold'>Knowladge Cafe</h1>
+     
+    </>
+  )
+}
+
+export default App
